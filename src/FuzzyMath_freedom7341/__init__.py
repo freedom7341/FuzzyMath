f15d8fe615df434e502dc6e778fc1f855f5a9c6d
Brady McDermott <freedom7341@hotmail.com>
2024-04-06 21:29:41 -0600

    add spaces around operators
    improves readability

--- src/FuzzyMath_freedom7341/__init__.py
+++ src/FuzzyMath_freedom7341/__init__.py
@@ -1,14 +1,15 @@
 import random
 import math
 
+# 
 fuzzy_get_sigfigs = lambda x : len(str(x).rstrip("0")) if len(str(x).split(".", 1)) == 1 else len(str(x).replace(".", "").lstrip("0"))
 fuzzy_get_bigfig = lambda x, y : x if (fuzzy_get_sigfigs(x) > fuzzy_get_sigfigs(y)) else y
 fuzzy_fuzz_num = lambda precision, sigfig : precision * (math.sqrt(sigfig) / 10)
 sign = lambda x: -1 if x < 0 else (1 if x > 0 else 0)
 
 def fuzzy_mult_loop(x, y, endType, precision=1):
-    a = int(x*(10**(fuzzy_get_sigfigs(x))))
-    b = int(y*(10**(fuzzy_get_sigfigs(y))))
+    a = int(x * (10 ** (fuzzy_get_sigfigs(x))))
+    b = int(y * (10 ** (fuzzy_get_sigfigs(y))))
     c = 0
     i = 0
     
@@ -17,11 +18,11 @@ def fuzzy_mult_loop(x, y, endType, precision=1):
         a = a << 1
         if (b % 2):
             c += a
-            c = c*(1+(sign(endType*((-1)**endType))*fuzzy_fuzz_num(precision, fuzzy_get_sigfigs(a))))
+            c = c * (1 + (sign(endType * ((-1) ** endType)) * fuzzy_fuzz_num(precision, fuzzy_get_sigfigs(a))))
         b = b >> 1
         i += 1
     
-    return c / (2*10**(fuzzy_get_sigfigs(x))*10**(fuzzy_get_sigfigs(y)))
+    return c / (2 * 10 ** (fuzzy_get_sigfigs(x)) * 10 ** (fuzzy_get_sigfigs(y)))
 
 def fuzzyarithmetic(operation, precision=1):
     match operation:
@@ -46,4 +47,4 @@ def fuzzyrange(x, y, operation ,precision=1):
         case 3: # Subtraction
             return (((x - y) * (1 - fuzzy_fuzz_num(precision, fuzzy_get_bigfig(x, y))), x - y,(x - y) * (1 + fuzzy_fuzz_num(precision, fuzzy_get_bigfig(x, y)))))
 
-print(fuzzyrange(fuzzyrange(5,5,1),5,1))
+#print(fuzzyrange(fuzzyrange(5,5,1),5,1))
